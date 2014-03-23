6ef3889038f1f48eda31f3dc35b69ccdb4e0bd62
EliCash82 <AlmostAnderson@gmail.com>
2014-03-23 15:31:32 -0400

    Added intro screen

---
+++ masque.rb
@@ -0,0 +1,33 @@
+require 'colorize'
+
+puts ""
+puts ""
+puts ""
+puts ""
+puts "************Anderson's HyperLit Engine************".white
+puts ""
+puts "" 
+puts "#######                  #     #                                                                            ".blue      
+puts "   #    #    # ######    ##   ##   ##    ####   ####  #    # ######     ####  ######    ##### #    # ###### ".light_blue
+puts "   #    #    # #         # # # #  #  #  #      #    # #    # #         #    # #           #   #    # #      ".blue
+puts "   #    ###### #####     #  #  # #    #  ####  #    # #    # #####     #    # #####       #   ###### #####  ".light_blue
+puts "   #    #    # #         #     # ######      # #  # # #    # #         #    # #           #   #    # #      ".blue
+puts "   #    #    # #         #     # #    # #    # #   #  #    # #         #    # #           #   #    # #      ".light_blue
+puts "   #    #    # ######    #     # #    #  ####   ### #  ####  ######     ####  #           #   #    # ###### ".blue
+puts ""
+puts ""
+puts "######                   ######                             ".red
+puts "#     # ###### #####     #     # ######   ##   ##### #    # ".light_red
+puts "#     # #      #    #    #     # #       #  #    #   #    # ".red
+puts "######  #####  #    #    #     # #####  #    #   #   ###### ".light_red
+puts "#   #   #      #    #    #     # #      ######   #   #    # ".red
+puts "#    #  #      #    #    #     # #      #    #   #   #    # ".light_red
+puts "#     # ###### #####     ######  ###### #    #   #   #    # ".red
+puts ""
+puts ""
+puts "Engine Narrative 1.0.0".white
+puts "Copyright 2014 by Anderson Evans".white
+puts ""
+puts "____________________________________________________________________________________________________________".blue
+
+
